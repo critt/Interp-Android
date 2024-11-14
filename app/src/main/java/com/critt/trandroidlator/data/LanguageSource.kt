d8bc1c18deba0befa6e2297680664b107fb98030
critt <cbarsim@gmail.com>
2024-11-14 15:31:25 -0800

    Added some utilities to LanguageSource for API response abstractions and converting Retrofit Calls to Flows.

--- app/src/main/java/com/critt/trandroidlator/data/LanguageSource.kt
+++ app/src/main/java/com/critt/trandroidlator/data/LanguageSource.kt
@@ -1,10 +1,61 @@
 package com.critt.trandroidlator.data
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface LanguageSource {
     @GET("getSupportedLanguages")
-    fun getSupportedLanguages(): Response<List<LanguageData>>
+    suspend fun getSupportedLanguages(): Response<List<LanguageData>>
 }
 
+sealed class ApiResult <out T> (val status: ApiStatus, val data: T?, val message:String?) {
+    data class Success<out R>(val _data: R?): ApiResult<R>(
+        status = ApiStatus.SUCCESS,
+        data = _data,
+        message = null
+    )
+
+    data class Error(val exception: String): ApiResult<Nothing>(
+        status = ApiStatus.ERROR,
+        data = null,
+        message = exception
+    )
+
+    data class Loading<out R>(val _data: R?, val isLoading: Boolean): ApiResult<R>(
+        status = ApiStatus.LOADING,
+        data = _data,
+        message = null
+    )
+}
+
+enum class ApiStatus {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
+
+fun <T> toResultFlow(call: suspend () -> Response<T>?) : Flow<ApiResult<T>?> {
+    return flow {
+        emit(ApiResult.Loading(null, true))
+        val c = call()  /* have to initialize the call method first*/
+        c?.let {
+            try {
+                if (c.isSuccessful && c.body() != null) {
+                    c.body()?.let {
+                        emit(ApiResult.Success(it))
+                    }
+                } else {
+                    c.errorBody()?.let {
+                        emit(ApiResult.Error(it.string()))
+                    }
+                }
+            }catch (e: Exception){
+                emit(ApiResult.Error(e.toString()))
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+}
