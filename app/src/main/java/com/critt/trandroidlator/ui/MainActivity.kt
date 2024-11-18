34800f17aad694af3429168338d4366175a04c9d
critt <cbarsim@gmail.com>
2024-11-18 14:58:11 -0800

    Comnpose imports. Removed a bunch of boilerplate resources from sample project creation. Initial compose setup in MainActivity.

--- app/src/main/java/com/critt/trandroidlator/ui/MainActivity.kt
+++ app/src/main/java/com/critt/trandroidlator/ui/MainActivity.kt
@@ -1,61 +1,56 @@
 package com.critt.trandroidlator.ui
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.critt.trandroidlator.R
-import com.critt.trandroidlator.databinding.ActivityMainBinding
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        setContent {
+            MainView()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    @Composable
+    @Preview
+    fun MainView() {
+        Column {
+            TranslationGroup("English", "German")
+            TranslationGroup("German", "English")
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    @Composable
+    fun TranslationGroup(subjectLanguage: String, objectLanguage: String) {
+        Column {
+            TranslationControls(subjectLanguage, objectLanguage)
+            OutputCard("Sample output")
+        }
+    }
+
+    @Composable
+    fun TranslationControls(subjectLanguage: String, objectLanguage: String) {
+        Row {
+            Text(subjectLanguage)
+            Text("⇌")
+            Text(objectLanguage)
+        }
+    }
+
+    @Composable
+    fun OutputCard(output: String) {
+        Text(output)
     }
 }
+
